9784a893df6d92207c449b15da441a66b0f158a7
takaharayota531 <takahara.yota@gmail.com>
2023-03-16 17:36:16 +0900

    create:user_company_relation_modelとpost_category_relation_model

---
+++ app/models/post_category_relation_model/post_skill_category_relationship.rb
@@ -0,0 +1,3 @@
+# 身につくスキルテーブル
+class PostCategoryRelationModel::PostSkillCategoryRelationship < ApplicationRecord
+end
---
+++ test/models/post_category_relation_model/post_job_category_relationship_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PostCategoryRelationModel::PostJobCategoryRelationshipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
