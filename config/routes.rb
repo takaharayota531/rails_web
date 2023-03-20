81c3ec1870ed0119c0be7daeec98172a0d7e557e
takaharayota531 <takahara.yota@gmail.com>
2023-03-20 15:36:44 +0900

    update:commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  namespace :companies do
-    get 'internships/create'
-    get 'internships/edit'
-    get 'internships/destroy'
-    get 'internships/index'
-    get 'internships/show'
-  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
