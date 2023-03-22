e5373f64ed24af0703ae2936c89d5bd50ed85408
takaharayota531 <takahara.yota@gmail.com>
2023-03-22 16:53:14 +0900

    update:rails testができない

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,6 +1,29 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
+  def setup
+    # @admin=Admin.new(name: "Example User", email: "user@example.com",
+    #   password: "foobar", password_confirmation: "foobar")
+    @admin=Admin.new(name: "Example User", email: "user@example.com")
+      
+  end
+
+
+  test "should be valid" do
+    puts @admin.name
+    assert @admin.valid?
+  end
+
+
+  # test "name should be present" do
+  #   @admin.name = "     "
+  #   assert_not @admin.valid?
+  # end 
+
+  # test "email should be present" do
+  #   @admin.email = "     "
+  #   assert_not @admin.valid?
+  # end
   # test "the truth" do
   #   assert true
   # end
