7b8b25040835a2b7a97cc6d32ad7370bee9ebc5f
takaharayota531 <takahara.yota@gmail.com>
2023-03-23 09:55:10 +0900

    update:devise向けのridgepoleファイル変更と テスト直し

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,15 @@ ActiveRecord::Schema[7.0].define(version: 0) do
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "password", null: false
+    t.string "email", default: "", null: false
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "companies", force: :cascade do |t|
