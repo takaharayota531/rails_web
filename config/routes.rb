9a58f19c5afb01bcfdad202c108c33211b1a62f5
takaharayota531 <takahara.yota@gmail.com>
2023-03-30 10:47:58 +0900

    update:

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   devise_for :company_accounts
   devise_for :companies
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    # sessions: 'users/sessions'
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema[7.0].define(version: 0) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -179,6 +179,15 @@ ActiveRecord::Schema[7.0].define(version: 0) do
     t.string "provider", null: false
     t.string "uid", null: false
     t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
