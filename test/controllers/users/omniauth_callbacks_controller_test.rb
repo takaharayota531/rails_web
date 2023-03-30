f62e74a172c9270b880ca14357f9ed8490ecb014
takaharayota531 <takahara.yota@gmail.com>
2023-03-30 14:41:40 +0900

    update:test書いてません

--- test/controllers/users/omniauth_callbacks_controller_test.rb
+++ test/controllers/users/omniauth_callbacks_controller_test.rb
@@ -1,7 +1,5 @@
 require "test_helper"
 
 class Users::OmniauthCallbacksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # TODO: 高原：テスト書いてない
 end
