385940d3cb69bca94fe011b08834d817c367929a
lucvil <yoshimura037@g.ecc.u-tokyo.ac.jp>
2023-03-31 11:00:33 +0900

    CREATE: should-mathersのgemを導入（外部キーテストを一行でかける） UPDATE: 外部キーテスト完了

--- spec/models/company_overall_spec.rb
+++ spec/models/company_overall_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe CompanyOverall, type: :model do
   context "associations" do
     it { should belong_to(:internship) }
   end
-
-  context "validations" do
-    it { should validate_presence_of(:internship) }
-  end
 end
